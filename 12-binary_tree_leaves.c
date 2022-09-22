6c32d099a4c5f73ee586b60eab49263a8befd011
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-09-22 17:39:47 +0300

    Corrction

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,14 +7,13 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t l = 0;
+
 	if (tree)
 	{
-		size_t l;
-
 		l += (!tree->left && !tree->right) ? 1 : 0;
 		l += binary_tree_leaves(tree->left);
 		l += binary_tree_leaves(tree->right);
 		return (l);
 	}
-	return (0);
 }
